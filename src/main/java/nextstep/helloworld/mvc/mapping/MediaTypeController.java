26cf6beab2ca4ea42dbbef0b4ce0e3af426a6dc8
Wannte <T.wannte@gmail.com>
2021-04-14 14:02:57 +0900

    feat: MediaTypeController 구현

--- src/main/java/nextstep/helloworld/mvc/mapping/MediaTypeController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/MediaTypeController.java
@@ -1,7 +1,6 @@
 package nextstep.helloworld.mvc.mapping;
 
 import nextstep.helloworld.mvc.domain.User;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +9,26 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/media-type")
+@RequestMapping("/media-type/users")
 public class MediaTypeController {
-
+    @PostMapping(consumes = "application/json")
     public ResponseEntity createUser(@RequestBody User user) {
         Long id = 1L;
-        return ResponseEntity.created(URI.create("/users/" + id)).build();
+        return ResponseEntity.created(URI.create("/users/" + id))
+                             .build();
     }
 
+    @GetMapping(produces = "application/json")
     public ResponseEntity<List<User>> showUser() {
         List<User> users = Arrays.asList(
                 new User("이름", "email"),
                 new User("이름", "email")
         );
-        return ResponseEntity.ok().body(users);
+        return ResponseEntity.ok()
+                             .body(users);
     }
 
+    @GetMapping(produces = "text/html")
     public String userPage() {
         return "user page";
     }

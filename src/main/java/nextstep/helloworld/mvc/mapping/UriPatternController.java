8ecf812b6fbfda24ccb19c46612117a248e66a5e
Wannte <T.wannte@gmail.com>
2021-04-14 14:37:44 +0900

    feat: UriPatternController 구현

--- src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/UriPatternController.java
@@ -2,22 +2,26 @@ package nextstep.helloworld.mvc.mapping;
 
 import nextstep.helloworld.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/uri-pattern")
 public class UriPatternController {
-
-    public ResponseEntity<User> pathVariable(Long id) {
+    @GetMapping(path = "/users/{id}")
+    public ResponseEntity<User> pathVariable(@PathVariable Long id) {
         User user = new User(id, "이름", "email");
         return ResponseEntity.ok().body(user);
     }
 
+    @GetMapping(path = "/patterns/?")
     public ResponseEntity<String> pattern() {
         return ResponseEntity.ok().body("pattern");
     }
 
+    @GetMapping(path = "/patterns/**")
     public ResponseEntity<String> patternStars() {
         return ResponseEntity.ok().body("pattern-multi");
     }

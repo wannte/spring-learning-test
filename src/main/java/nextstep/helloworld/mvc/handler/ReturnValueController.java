a3e5bd85ec25c5167a25dfe82c0ad46b2eac368f
Wannte <T.wannte@gmail.com>
2021-04-17 19:52:33 +0900

    feat: ReturnValueController 구현

--- src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
@@ -3,25 +3,33 @@ package nextstep.helloworld.mvc.handler;
 import nextstep.helloworld.mvc.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/return-value")
 public class ReturnValueController {
+    @GetMapping("/message")
+    @ResponseBody
+    public String string() {
+        return "message";
+    }
 
-//    public void string() {
-//        return "message";
-//    }
-//
-//    public void responseBodyForUser() {
-//        return new User("name", "email");
-//    }
-//
-//    public void responseEntity(@PathVariable Long id) {
-//        return ResponseEntity.ok(new User("name", "email"));
-//    }
-//
-//    public void responseEntityFor400() {
-//        return ResponseEntity.badRequest().build();
-//    }
+    @GetMapping("/users")
+    @ResponseBody
+    public User responseBodyForUser() {
+        return new User("name", "email");
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> responseEntity(@PathVariable Long id) {
+        return ResponseEntity.ok(new User("name", "email"));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity responseEntityFor400() {
+        return ResponseEntity.badRequest().build();
+    }
 }
